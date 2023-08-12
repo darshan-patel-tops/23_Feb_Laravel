534d06e9b270683125b4d436428891d3f37e8788
darshan <darshan.patel.tops@gmail.com>
2023-08-12 16:03:56 +0530

    Update

---
+++ project/resources/views/welcome.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Laravel</title>
+</head>
+<body>
+<center>
+
+
+    <h1>Welcome to the lecture of laravel</h1>
+
+</center>
+
+</body>
+</html>
